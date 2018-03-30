c7070fc2d774f9c7da95c5a3a3a6030934f0f95d
unknown <uzuchowicz@gmail.com>
2018-03-30 16:58:11 +0200

    read excel data

---
+++ EEG_data_analysis.py
@@ -0,0 +1,18 @@
+#from openpyxl import load_workbook
+import pandas as pd
+import numpy as np
+
+filename='SpectralAnalysis.xlsx'
+sheetname='PowerSpectrum_all_s1_s2'
+path="data\\"
+
+#Read excel data
+# excel_data = pd.ExcelFile(path+filename)
+# data = excel_data.parse(sheetname).head(0)
+# labels=tuple(data.head(0))
+# power_spectrum_data=np.matrix(excel_data.parse(sheetname, skiprows =1))
+
+excel_data=pd.read_excel(path+filename, skiprows=0)
+labels=pd.read_excel(path+filename, header=0)
+labels=tuple(labels)
+excel_data=np.matrix(excel_data)
